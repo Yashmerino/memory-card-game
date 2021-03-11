24d4d783378b5b378cab22158502d48ba49fe52e
Artiom Bozieac <artiomytprofile@gmail.com>
2021-03-11 22:52:46 +0200

    Add project files.

---
+++ Pexeso.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1024.f, 768.f), "Pexeso");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while(window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		//
+	}
+
+	return 0;
+}
